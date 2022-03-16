84df8e1c85f2b6b4a51e68a4f6075beddc8af87e
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2022-03-16 16:05:09 -0300

    chore: fixed indent

--- src/source.py
+++ src/source.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+
 import numpy as np
 
 
@@ -10,16 +11,17 @@ class Source:
 
     @property
     def output(self) -> List[str]:
-        return self._input
+        return self._output
 
-  def generate_messages(self, n: int = None) -> None:
-    if n is None:
-        raise AttributeError("No amount of messages to generate provided")
+    def generate_messages(self, n: int = None) -> None:
+        if n is None:
+            raise AttributeError("No amount of messages to generate provided")
 
-    self._output = self._rng.choice(list(self.alphabet.keys()), n, p=list(self.alphabet.values()))
+        self._output = self._rng.choice(
+            list(self.alphabet.keys()), n, p=list(self.alphabet.values())
+        )
 
 
 class UniformSource(Source):
     def __init__(self, *symbols: str) -> None:
-        super().__init__({symbol:1 / len(symbols) for symbol in symbols})
-
+        super().__init__({symbol: 1 / len(symbols) for symbol in symbols})
