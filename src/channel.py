7c3f25743519dad8478ec3e5b7f2e8d8152466a8
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2022-03-16 16:05:40 -0300

    chore: channel takes source as input, produces correct output

--- src/channel.py
+++ src/channel.py
@@ -1,10 +1,14 @@
-from typing import List, Dict, Callable
+from typing import Any, Callable, Dict, List, Union
+
+import numpy as np
+from numpy import ndarray
 
 from source import Source
 
+
 class Channel:
-    def __init__(self, input: Source=None) -> None:
-        self._input = input
+    def __init__(self, input_source: Source = None) -> None:
+        self._input = input_source
 
     @property
     def input(self) -> List[str]:
@@ -19,25 +23,29 @@ class Channel:
         self._input = None
 
     @property
-    def output(self) -> List[float]:
-        raise NotImplementedError("An output function needs to be implemented for this channel.")
+    def output(self) -> List[Any]:
+        raise NotImplementedError(
+            "An output function needs to be implemented for this channel."
+        )
 
     def mapper(self) -> Callable:
-        raise NotImplementedError("A mapper function needs to be implemented for this channel.")
+        raise NotImplementedError(
+            "A mapper function needs to be implemented for this channel."
+        )
 
 
 class PoissonChannel(Channel):
-    def __init__(self, dist: Dict[str, float]) -> None:
+    def __init__(self, input_source: Source, dist: Dict[str, float]) -> None:
         self._params = dist
-        super().__init__()
+        super().__init__(input_source)
 
     @property
     def params(self) -> Dict[str, float]:
         return self._params
 
-    def mapper(self) -> Callable:
-        return np.random.poisson
+    def mapper(self, *args, **kwargs) -> Union[ndarray, int, float, complex]:
+        return np.random.poisson(*args, **kwargs)
 
     @property
-    def output(self) -> List[float]:
+    def output(self) -> list[Any]:
         return [self.mapper(self._params[x]) for x in self.input]
