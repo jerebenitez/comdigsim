f7ebd1b2d1f0246ca5ad4a21e5157006c4f7e569
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2022-03-17 15:35:37 -0300

    feat: receiver should make a prediction for all incoming messages

--- tests/test_receiver.py
+++ tests/test_receiver.py
@@ -1,7 +1,7 @@
 import unittest as ut
 
 from src.channel import PoissonChannel
-from src.receiver import Receiver
+from src.receiver import MAPReceiver, Receiver
 from src.source import UniformSource
 
 
@@ -15,3 +15,15 @@ class TestReceiver(ut.TestCase):
         r = Receiver(source=self.source, channel=self.channel)
         with self.assertRaises(NotImplementedError):
             _ = r.output
+
+
+class TestMAPReceiver(ut.TestCase):
+    def setUp(self) -> None:
+        self.no_of_messages = 10
+        self.source = UniformSource(["0", "1"])
+        self.source.generate_messages(self.no_of_messages)
+        self.channel = PoissonChannel(input_source=self.source, lambdas=[1.5, 15])
+
+    def test_should_make_a_prediction_for_all_messages(self) -> None:
+        r = MAPReceiver(source=self.source, channel=self.channel)
+        self.assertEqual(len(r.output), self.no_of_messages)
