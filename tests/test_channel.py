b2d5d324e6ee990a7519dd85abb6f57a29f9ba57
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2022-03-17 15:26:32 -0300

    feat: channel accepts sources of different types

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,5 +1,7 @@
 import unittest as ut
 
+import numpy as np
+
 from src.channel import Channel, PoissonChannel
 from src.source import UniformSource
 
@@ -36,3 +38,10 @@ class TestPoissonChannel(ut.TestCase):
     def test_fail_on_incorrect_lambdas(self) -> None:
         with self.assertRaises(AttributeError):
             _ = PoissonChannel(input_source=self.source, lambdas=[])
+
+    def test_channel_accepts_integer_sources(self) -> None:
+        s = UniformSource([1, 2])
+        c = PoissonChannel(input_source=s, lambdas=self.lambdas)
+        s.generate_messages(10)
+        # np.int64 is used because, internally, the source calls numpy to select its output
+        self.assertEqual(all(isinstance(x, np.int64) for x in c.input), True)
