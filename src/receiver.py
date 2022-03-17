9a776d56d43a057f94ad87e9b201af06ffe17bfc
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2022-03-17 15:50:28 -0300

    chore: receiver predicts symbol sent from signal

--- src/receiver.py
+++ src/receiver.py
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import Any, Dict, List
 
 from src.channel import Channel
 from src.source import Source
@@ -13,10 +13,26 @@ class Receiver:
     def output(self) -> List[Any]:
         return self.hypothesis_test(self.channel.output)
 
-    def hypothesis_test(self, symbols: List[Any]) -> List[Any]:
+    def hypothesis_test(self, inputs: List[Any]) -> List[Any]:
         raise NotImplementedError("No hypothesis testing method was defined.")
 
 
 class MAPReceiver(Receiver):
-    def hypothesis_test(self, symbol) -> List[str]:
-        pass
+    def hypothesis_test(self, inputs: List[Any]) -> List[Any]:
+        predicted_probability = 0
+        predicted_symbol = None
+        predicted_symbols = []
+        for _input in inputs:
+            input_probability = self.channel.get_probability_for(_input)
+            for symbol in self.source.alphabet:
+                probability = (
+                    self.source.get_probability_for(symbol) * input_probability
+                )
+                if probability > predicted_probability:
+                    predicted_probability = probability
+                    predicted_symbol = symbol
+            predicted_symbols.append(predicted_symbol)
+            predicted_probability = 0
+            predicted_symbol = None
+
+        return predicted_symbols
